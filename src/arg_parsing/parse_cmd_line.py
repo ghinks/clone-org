7a2f38b4ca196e46330dafd67194f112d9e91fb6
Glenn Hinks <ghinks@yahoo.com>
2023-02-20 15:15:10 -0500

    feature: argparse
    tidy up variable names for arg passing

--- src/arg_parsing/parse_cmd_line.py
+++ src/arg_parsing/parse_cmd_line.py
@@ -21,30 +21,29 @@ def parse_cmd_line():
     org_help = """
     The organization in Github that you wish to clone all the repositories for.
     """
-    parser.add_argument('-o', '--org', required=True, help=org_help)
+    parser.add_argument('-o', '--organization', required=True, help=org_help)
     proto_help = """
     The protocol to use either https , which will require GITHUB_TOKEN to be defined
     in your environment variables. Or ssh which will require that you have your ssh
     keys set up in current shell.
     """
-    parser.add_argument('-p', '--prot', type=str, choices=["https", "ssh"], default="https", help=proto_help)
+    parser.add_argument('-p', '--protocol', type=str, choices=["https", "ssh"], default="https", help=proto_help)
     to_folder_help = """
     The target folder should be a fully qualified name and the directory structures tested are OSx and Linux.
     An example would be -f /home/Users/alice/dev/
     """
-    parser.add_argument("-f", "--fol", type=str, default=os.getcwd(), help=to_folder_help)
+    parser.add_argument("-f", "--folder", type=str, default=os.getcwd(), help=to_folder_help)
     args = parser.parse_args()
-    print(args)
     url = get_url_type(args)
-    if not check_folder_exists(args.fol):
-        raise ValueError(f"Folder {args.fol} does not exist")
-    return dict([("org", args.org), ("url_proto", url), ("to_folder", args.fol)])
+    if not check_folder_exists(args.folder):
+        raise ValueError(f"Folder {args.folder} does not exist")
+    return dict([("org", args.organization), ("url_proto", url), ("to_folder", args.folder)])
 
 def check_folder_exists(to_folder):
     return os.path.isdir(to_folder)
 
 def get_url_type(args):
     url = "url"
-    if args.prot == "ssh":
+    if args.protocol == "ssh":
         url = "sshUrl"
     return url
--- src/main.py
+++ src/main.py
@@ -2,6 +2,16 @@ from pprint import pprint as pp
 from src.read_github_namespace.graph.fetch_org_repos import collate, fetch_org_repos
 from src.clone.shell_clone import clone_list
 from src.arg_parsing.parse_cmd_line import parse_cmd_line
+from tabulate import tabulate
+
+
+def tabulate_nodes(nodes, url_protocol):
+    table = []
+    for node in nodes:
+        row = [node["name"], node[url_protocol]]
+        table.append(row)
+    print(tabulate(table, headers=["name", "url"], tablefmt="github"))
+
 
 if __name__ == "__main__":
     args = parse_cmd_line()
@@ -10,5 +20,6 @@ if __name__ == "__main__":
     to_folder = args["to_folder"]
     results = fetch_org_repos(organization)
     nodes = collate(results)
-    pp(nodes)
-    #clone_list(nodes, to_folder, "sshUrl")
+    # pp(nodes)
+    tabulate_nodes(nodes, url_protocol)
+    # clone_list(nodes, to_folder, "sshUrl")
