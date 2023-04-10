ee9586ef1c6c78204f7f2af5251117b08dfdb35a
Glenn Hinks <ghinks@yahoo.com>
2023-04-10 08:38:58 -0400

    feat: truthy / falsey for dict key
    add comments

--- src/read_namespace/utils/check_dict.py
+++ src/read_namespace/utils/check_dict.py
@@ -1,4 +1,16 @@
 def nested_keys_exist(dictionary, keys):
+    """Detect if the nested key exists in the dictionary
+
+    This is a general test allough intended for use with dictionaries
+    created from json files. We cannot guarantee that the keys are going
+    to be extant.
+
+    Args:
+        dictionory: a dictionary to be tested
+        keys: the keychain to walk to test if all the names exist
+
+    Returns: True or False
+    """
     if type(dictionary) is not dict:
         raise TypeError("Dictionary type expected")
     if type(keys) is not list:
